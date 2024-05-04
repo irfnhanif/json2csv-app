5566baf8cf4a46c8195ba5338548c2400fa05927
irfnhanif <14irfanh@gmail.com>
2024-05-04 16:32:53 +0700

    Update HTML form in index.html and add JSON to CSV conversion logic in converter.js

--- converter.js
+++ converter.js
@@ -1,15 +1,24 @@
-function convertJSONToCSV(jsonInput) {
-  document
-    .getElementById("jsonForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      const jsonInput = document.getElementById("jsonInput").value;
+function convertJSONToCSV(parsedJSON) {
+  if (typeof parsedJSON === "object" && !Array.isArray(parsedJSON)) {
+    const fields = Object.keys(parsedJSON);
+    let fieldString = fields.join(",");
+    let rowString = fields.map((field) => parsedJSON[field]).join(",");
+    fieldString += "\n";
+    return fieldString + rowString;
+  }
 
-      try {
-        JSON.parse(jsonInput);
-        // add json parsing to csv logic
-      } catch (error) {
-        return error.message;
-      }
-    });
+  let fields;
+  let fieldString;
+  let rowString = "";
+  for (let i = 0; i < parsedJSON.length; i++) {
+    if (i === 0) {
+      fields = Object.keys(parsedJSON[i]);
+      fieldString = fields.join(",") + "\n";
+    }
+
+    let currentRow = fields.map((field) => parsedJSON[i][field]).join(",");
+    rowString += currentRow + "\n";
+  }
+
+  return fieldString + rowString;
 }
